8a3135959284f5274c7da184705c251d2f1310d3
Veysel Aydın ERALP <veyselaydineralp@gmail.com>
2024-08-13 11:41:28 +0300

    Kare Ve Kup

---
+++ KareVeKupAl/KareVeKupAl/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace KareVeKupAl
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // Kullanıcıdan bir sayı girmesini isteriz
+                Console.Write("Bir sayı girin: ");
+                int number = int.Parse(Console.ReadLine());
+
+                // Sayının karesini hesaplar
+                int square = number * number;
+                // Sayının küpünü hesaplar
+                int cube = number * number * number;
+
+                // Sonuçları ekrana yazdırır
+                Console.WriteLine($"Girilen sayının karesi: {square}");
+                Console.WriteLine($"Girilen sayının küpü: {cube}");
+            }
+            catch (FormatException)
+            {
+                // Eğer geçersiz bir giriş yapılırsa hata mesajı gösterecektie.
+                Console.WriteLine("Geçerli bir tamsayı girin!");
+            }
+        }
+    }
+}
+
